62221ade03d1f033869fbcdb08491f12cdf0a946
deepparrot <53057954+deepparrot@users.noreply.github.com>
2019-10-03 14:36:38 +0100

    Create sotabench_setup.sh

---
+++ sotabench_setup.sh
@@ -0,0 +1,5 @@
+apt-get install unzip
+tar -zxvf ./.data/vision/imagenet/ILSVRC2012_devkit_t12.tar.gz -C ./.data/vision/imagenet/ILSVRC2012_devkit_t12/
+tar -zxvf ./.data/vision/imagenet/ILSVRC2012_img_val.tar -C ./.data/vision/imagenet/ILSVRC2012_img_val/
+ls
+echo "Dataset downloaded."
